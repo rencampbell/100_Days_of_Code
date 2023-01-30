06ccc5fad609c87ed73a7ab62b3f8d011d92ff0f
rencampbell <renee.r.d.campbell@gmail.com>
2023-01-30 09:22:36 -0500

    Fifth commit

--- day01/main.py
+++ day01/main.py
@@ -1,3 +1,4 @@
+# This program creates a band name by concatenating two strings, stored in variables
 print("Hey! Let's create the perfect band name!")
 city=input("Where did you grow up? ")
 pet=input("What's your pet's name? ")
