db0e8624ff77c4000131f379704f1be5fdde2fab
rencampbell <renee.r.d.campbell@gmail.com>
2023-01-31 15:28:35 -0500

    third commit

--- day07/main.py
+++ day07/main.py
@@ -1,8 +1,9 @@
 import random
-
-word_list=["ardvark","baboon","camel"]
+from hangman_words import word_list
+from hangman_art import stages, logo2, logo3
 
 display=[]
+lives=6
 
 def select_word (list):
     word=random.choice(list)
@@ -32,6 +33,7 @@ def update_display (guess,word):
     return(display)
 
 
+
 chosen_word=select_word(word_list)
 print(chosen_word)
 
@@ -45,9 +47,16 @@ while not end_of_game:
     user_guess=input("Guess a letter: ").lower()
 
     is_letter=find_letter(chosen_word,user_guess)
+    if is_letter==False:
+        lives-=1
+        if lives==0:
+            end_of_game=True
+            print("You lose!")
 
     new_display=update_display(user_guess,chosen_word)
     print(new_display)
+
+    print(stages[lives])
 
     if "_" not in new_display:
         end_of_game=True
