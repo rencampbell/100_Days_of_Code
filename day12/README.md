317f5be52124558b34cb275fd2d312e552722bd0
rencampbell <renee.r.d.campbell@gmail.com>
2023-02-11 17:29:09 -0500

    second commit

--- day12/README.md
+++ day12/README.md
@@ -5,5 +5,7 @@
 -Python Constants and Global Scope\
 
 ## Activities executed:
-A number guessing game was created according to the following rules:
--The \
+A number guessing game was created where:
+-A random answer between 1 and 100 was generated\
+-The user was allowe to choose between level hard (5 chances) and level easy (10 chances)\
+-The user was given hits in the form of 'too high' or 'too low' until their chances were finished or they guessed correctly
--- day12/main.py
+++ day12/main.py
@@ -1,30 +1,38 @@
 import random
 from art import logo
 
+#prints logo from the art module
 print(logo)
 print("I'm thinking of a number between 1 and 100...")
+#generates answer as random number between 1 and 100
 answer=random.randint(1,100)
-print(answer)
 
+#allows the user to choose their level of difficulty
 difficulty=input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
+#if the user gets 10 chances if they select easy and 5 chances if they select hard
 if difficulty=="easy":
     attempts=10
 else:
     attempts=5
 
-game_over=False
-
-while not game_over:
-
-    while attempts>0:
-        print(f"You have {attempts} attempts remaining to guess the number.")
-        guess=input("Make a guess: ")
-        if guess==answer:
-            print("That's right! The correct answer was {answer}. YOU WIN!")
-            game_over==True
+#while the user has not exhausted their attempts
+while attempts>0:
+    #prints the number of attempts remaining
+    print(f"\nYou have {attempts} attempts remaining to guess the number.")
+    #allows the user to guess what the answer is
+    guess=int(input("Make a guess: "))
+    #if they guess correctly a congratulatory message appears and the game is ended
+    if guess==answer:
+        print(f"That's right! The correct answer was {answer}. YOU WIN!")
+        attempts=0
+    #if they guess incorrectly they are given a hint
+    else:
+        if guess>answer:
+            print("Too high.")
         else:
-            if guess>answer:
-                print("Too high.")
-            else:
-                print("Too low.")
-            attempts-=1        
+            print("Too low.")
+        #if they guess incorrectly an attempt is subtracted
+        attempts-=1
+        #if attempts=0 a message states that the user lost
+        if attempts==0:
+            print(f"\nThe correct answer was {answer}. YOU LOSE!")
