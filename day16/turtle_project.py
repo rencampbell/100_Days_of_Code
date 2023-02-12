2c24b6a799a685f9a340193749fbe85886e07818
rencampbell <renee.r.d.campbell@gmail.com>
2023-02-12 12:35:40 -0500

    first commit

--- day16/turtle_project.py
+++ day16/turtle_project.py
@@ -2,7 +2,11 @@ from turtle import Turtle, Screen
 
 timmy=Turtle()
 print(timmy)
+timmy.shape("turtle")
+timmy.color("green")
+timmy.forward(100)
 
 my_screen=Screen()
 print(my_screen.canvheight)
+my_screen.exitonclick()
 
