bd72691f78f7837e532d6e4489fc0803720f5b03
rencampbell <renee.r.d.campbell@gmail.com>
2023-03-26 15:10:32 -0500

    task_classes done

--- python_3/test_task_3.py
+++
@@ -1,9 +0,0 @@
-from python_3 import is_http_domain
-import pytest
-
-
-def test_is_domain():
-    assert is_http_domain('https://ru.wikipedia.org/') == True
-
-def test_is_not_domain():
-    assert is_http_domain('griddynamics.com') == False
